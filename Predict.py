826201c9efa25a81f1fe123daf52dfc25c16b6b3
Abhi bhosale <bhosaleabhishek379@gmail.com>
2026-01-17 15:06:39 +0530

    pkl

--- Predict.py
+++ Predict.py
@@ -1,9 +1,7 @@
 import joblib
-import numpy as np
+import os
 
-model = joblib.load("house_price_model.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "house_price_model.pkl")
 
-def predict_price(input_data):
-    input_array = np.array(input_data).reshape(1, -1)
-    prediction = model.predict(input_array)
-    return prediction[0]
+joblib.dump(model, MODEL_PATH)
